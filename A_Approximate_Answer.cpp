54e34acf7fffafc47860482513fecebfb0fa4510
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-10-08 14:05:56 +0300

    updated

--- A_Approximate_Answer.cpp
+++ A_Approximate_Answer.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 int main()
 {
-    
+    int X, Y, K;
+    cin >> X >> Y >> K;
+    if(abs(X-Y) > K){
+        cout << "No";
+    }
+    else{
+        cout << "Yes";
+    }
     return 0;
 }
